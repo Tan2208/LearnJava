8c2ff03fc9eecacec5a7ae9a3cdbc315c9f5d2a2
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-02-18 16:59:04 +0700

    Variable in Java

---
+++ src/Java04/Variable.java
@@ -0,0 +1,11 @@
+package Java04;
+
+public class Variable {
+    public static void main(String[] args) {
+        String tB = "Hello";
+        String fullName = "Trần Duy Tân";
+
+        System.out.println("Thông báo:"+tB);
+        System.out.println("Họ và tên:"+fullName);
+    }
+}
