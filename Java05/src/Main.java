cd9b1f53b1199f8f0ab1766a30673a36581a6650
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-02-18 17:29:06 +0700

    Java data type

--- Java05/src/Main.java
+++ Java05/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String nameBook = "Lập trình Java";
+        int year = 2020;
+        double price = 12.5;
+        boolean flag = true;
+        char id = 'C';
+        System.out.println("Name: " + nameBook);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+        System.out.println("Flag: " + flag);
+        System.out.println("ID: " + id);
     }
 }
