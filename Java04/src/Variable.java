ba632de39340e5e5e23440e9d3823346c60ec577
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-02-18 17:16:54 +0700

    Change file

---
+++ Java04/src/Variable.java
@@ -0,0 +1,10 @@
+
+public class Variable {
+    public static void main(String[] args) {
+        String tB = "Hello";
+        String fullName = "Trần Duy Tân";
+
+        System.out.println("Thông báo:"+tB);
+        System.out.println("Họ và tên:"+fullName);
+    }
+}
