c11b27f013596a532030eafb5427d27db5b7442a
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-02-22 20:40:40 +0700

    Java25: Do...while in Java

---
+++ Java25/src/Java25.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Java25 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        do
+        {
+            System.out.print("Enter number: ");
+            n = sc.nextInt();
+        }while (n != 0);
+    }
+}
