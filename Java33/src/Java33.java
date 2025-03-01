69ca39d68b7c68cceaea89d2e8d5e4a5edb07ef9
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-03-01 16:36:50 +0700

    Java33: To String in Java

---
+++ Java33/src/Java33.java
@@ -0,0 +1,13 @@
+public class Java33 {
+    public static void main (String[] args)
+    {
+        MyDate33 md = new MyDate33(14,1,2025);
+        MyDate33 md2 = new MyDate33(4,10,2025);
+        MyDate33 md3 = new MyDate33(24,5,2025);
+
+        System.out.println(md);
+        System.out.println(md2);
+        System.out.println(md3);
+
+    }
+}
