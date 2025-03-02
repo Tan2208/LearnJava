eeadb32f1c65ad72bde4e61e0dcaf47a7a3e8485
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-03-02 17:34:42 +0700

    Java43: Overloading in Java

---
+++ Java43/src/Java43.java
@@ -0,0 +1,10 @@
+public class Java43 {
+
+    public static void main(String[] args)
+    {
+        Min m=new Min();
+        System.out.println("Min:"+m.findMin(6,5));
+        System.out.println("Min:"+m.findMin(1.5,2.5));
+
+    }
+}
---
+++ Java43/src/Min.java
@@ -0,0 +1,20 @@
+public class Min {
+
+    public int findMin(int a, int b)
+    {
+        if(a>b)
+            return b;
+        else
+            return a;
+    }
+
+    public double findMin(double a, double b)
+    {
+        if(a>b)
+            return b;
+        else
+            return a;
+    }
+
+
+}
