7210729f8e61d1d35a847fa39aed5475031e2496
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-03-08 19:53:33 +0700

    Java51:String in Java

---
+++ Java51/src/Java51.java
@@ -0,0 +1,21 @@
+public class Java51 {
+    public static void main(String[] args)
+    {
+        String s1 = "Hello world, Hello";
+        String s2 = "Hello";
+        String s3 = "Hello 123";
+        char c1 = 'r';
+        //Function indexOf
+        System.out.println("Position of s2 in s1: "+ s1.indexOf(s2));
+        System.out.println("Position of s3 in s1: "+ s1.indexOf(s3));
+
+        System.out.println("Position of s2 in s1: "+ s1.indexOf(s2,1));
+
+        System.out.println("Position of c2 in s1: "+ s1.indexOf(c1));
+        System.out.println("Position of c2 in s1: "+ s1.indexOf(c1,9));
+        System.out.println("--------------");
+        //Function lastIndexOf : right -> left
+        System.out.println("Position of s2 in s1: "+ s1.lastIndexOf(s2));
+
+    }
+}
